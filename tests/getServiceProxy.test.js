f7b963e6aabbe15980e3574212e25e6360fa47ac
Fabio Percivaldi <fabio-percivaldi@mia-platform.eu>
2020-05-01 12:47:23 +0200

    updated doc and added a test on getDirectServiceProxy with custom port

--- tests/getServiceProxy.test.js
+++ tests/getServiceProxy.test.js
@@ -29,6 +29,29 @@ tap.test('getDirectServiceProxy available for testing', async t => {
   awesomeServiceScope.done()
 })
 
+tap.test('getDirectServiceProxy accept all options', async t => {
+  nock.disableNetConnect()
+  t.tearDown(() => {
+    nock.enableNetConnect()
+  })
+
+  const RETURN_MESSAGE = 'OK'
+  const customProxy = getDirectServiceProxy(MY_AWESOME_SERVICE_PROXY_URL, {
+    port: 3000,
+  })
+
+  const awesomeServiceScope = nock(`http://${MY_AWESOME_SERVICE_PROXY_URL}:3000`)
+    .get('/test-endpoint')
+    .reply(200, {
+      message: RETURN_MESSAGE,
+    })
+
+  const result = await customProxy.get('/test-endpoint')
+
+  t.strictSame(result.statusCode, 200)
+  t.strictSame(result.payload.message, RETURN_MESSAGE)
+  awesomeServiceScope.done()
+})
 
 tap.test('getServiceProxy available for testing', async t => {
   nock.disableNetConnect()
