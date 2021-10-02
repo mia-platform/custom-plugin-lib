6ca74ae8f167df1a457ee22db7077d46f0c9aa86
Davide Bianchi <bianchidavide12@gmail.com>
2021-10-02 22:38:59 +0200

    test: add test on oas to check it is correctly generated

---
+++ tests/oas.test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const tap = require('tap')
+const lc39 = require('@mia-platform/lc39')
+const Swagger = require('swagger-parser')
+
+async function setupFastify(filePath, envVariables) {
+  const fastify = await lc39(filePath, {
+    logLevel: 'silent',
+    envVariables,
+  })
+
+  return fastify
+}
+
+const USERID_HEADER_KEY = 'userid-header-key'
+const GROUPS_HEADER_KEY = 'groups-header-key'
+const CLIENTTYPE_HEADER_KEY = 'clienttype-header-key'
+const BACKOFFICE_HEADER_KEY = 'backoffice-header-key'
+const MICROSERVICE_GATEWAY_SERVICE_NAME = 'microservice-gateway'
+
+const baseEnv = {
+  USERID_HEADER_KEY,
+  GROUPS_HEADER_KEY,
+  CLIENTTYPE_HEADER_KEY,
+  BACKOFFICE_HEADER_KEY,
+  MICROSERVICE_GATEWAY_SERVICE_NAME,
+}
+
+tap.test('support $ref schema', async t => {
+  const fastify = await setupFastify('./tests/services/plain-custom-service.js', baseEnv)
+
+  const res = await fastify.inject({
+    method: 'GET',
+    url: '/documentation/json',
+  })
+
+  await Swagger.validate(JSON.parse(res.payload))
+
+  t.end()
+})
